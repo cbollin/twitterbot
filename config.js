f79d27e4e9ec0d9da5d73648884764154f8bf177
Cassidy Bollin <cassidybollin32@gmail.com>
2017-01-06 16:15:25 -0800

    cleaning up

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   consumer_key:         'JPkeTGaZHZIap7xQb99RDRdw6',
-  consumer_secret:      'IFK8VMpopJdkEEdJsoAPxzP4xu68E509L1Kuv4Gw4lC1ibOAvJ',
-  access_token:         '754022101988716545-17mbIuQUt8OGh5bQf2RpNiHtrGFfoGS',
-  access_token_secret:  '5HUXff5dyj7v6Uf4X10rfwyKQDVRlYRzuwRvFrGHujUb5',
+  consumer_secret:      'INSERT HERE',
+  access_token:         'HERE-HERE',
+  access_token_secret:  'AND HERE',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
